17b98349158ff2c29834f0cf88a0eec626788d35
abdilatifbashir <abdiladifbashir0000@gmail.com>
2021-03-23 02:41:08 +0300

    fix lint issue

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -122,9 +122,18 @@
             </div>
             <div class="flex mr-10">
               <span
-                ><svg class=" w-4 pt-1 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-  <path fill-rule="evenodd" d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-</svg>
+                ><svg
+                  class=" w-4 pt-1 text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
               </span>
               <span class="bg-gray-300 w-4 h-2 rounded-full mt-2 ml-2"></span>
             </div>
